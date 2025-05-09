ea141409c44113977254ad238d8fecee273555e9
Miryl <hntb0102@gmail.com>
2025-05-09 15:43:47 +0700

    update header ui

--- frontend/src/components/layout/SideBar.jsx
+++ frontend/src/components/layout/SideBar.jsx
@@ -2,6 +2,8 @@ export default function Sidebar() {
   return (
     <aside className="fixed top-16 left-0 w-64 h-[calc(100vh-64px)] bg-black text-white flex flex-col z-20">
       
+        
+      
     </aside>
   );
 }
