b7fe39fea51bb5ab82a73fbf5fffdd9ee73fb81d
An <antran25722@gmail.com>
2025-05-12 13:42:37 +0700

    GET RESAPI

--- backend/spotify/serializer.py
+++ backend/spotify/serializer.py
@@ -1,8 +1,58 @@
+# serializers.py
 from rest_framework import serializers
-from .models import Track
+from .models import User, Artist, Album, Track, Playlist, PlaylistTrack, Like, Follower, Premium, UserPremium, Payment
 
-########## Track Serializer ##########
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'  # This will serialize all fields of the Artist model
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = '__all__'
+
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
+        fields = '__all__'
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = '__all__'
+
+class PlaylistTrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlaylistTrack
+        fields = '__all__'
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = '__all__'
+
+class FollowerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follower
+        fields = '__all__'
+
+class PremiumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Premium
+        fields = '__all__'
+
+class UserPremiumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPremium
+        fields = '__all__'
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
