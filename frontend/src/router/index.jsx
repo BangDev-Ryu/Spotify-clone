ca1e6b62b73a9bbd39e0c9c0fe7c6b34c00d8402
BangDev-Ryu <ryushiki12@gmail.com>
2025-05-12 21:41:10 +0700

    update router

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,37 +1,53 @@
 import { createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "../components/layout/DefaultLayout";
 import MainContent from "../components/pages/MainContent";
 import Track from "../components/pages/Track";
 import AlbumPage from "../components/pages/AlbumPage";
 import PlaylistPage from "../components/pages/PlaylistPage";
-
+import Login from "../components/pages/Login";
+import Register from "../components/pages/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainContent />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <MainContent />,
+      },
+      {
+        path: "/track",
+        element: <Track />, 
+      },
+      {
+        path: "/track/:id",
+        element: <Track />,
+      },
+      {
+        path: "/album",
+        element: <AlbumPage />,
+      },
+      {
+        path: "/album/:id",
+        element: <AlbumPage />,
+      },
+      {
+        path: "/playlist",
+        element: <PlaylistPage />,
+      },
+      {
+        path: "/playlist/:id",
+        element: <PlaylistPage />,
+      }
+    ]
   },
   {
-    path: "/track",
-    element: <Track />,
+    path: "/login",
+    element: <Login />
   },
   {
-    path: "/track/:id",
-    element: <Track />,
-  },
-  {
-    path: "/album",
-    element: <AlbumPage />,
-  },
-  {
-    path: "/album/:id",
-    element: <AlbumPage />,
-  },
-  {
-    path: "/playlist",
-    element: <PlaylistPage />,
-  },
-  {
-    path: "/playlist/:id",
-    element: <PlaylistPage />,
-  },
+    path: "/register",
+    element: <Register />
+  }
 ]);
